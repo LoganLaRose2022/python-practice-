ed51464769ec5e8f47c35c7431266a5d53229f1b
LoganLaRose2022 <814107@mystma.org>
2021-09-16 15:19:18 -0500

    add madlib

--- main.py
+++ main.py
@@ -1,8 +1,58 @@
-#Print first movie
-print("Chicken Little")
+def topfivemovies():
+    print("cars")
+    print("chicken little")
+    print("frozen")
 
-#Print 2nd movie 
-print("Cars")
+def topfivesports():
+    print("football")
+    print("bowling")
+    print("soccer")
 
-#Print 3rd movie
-print("Over the Hedge")
+def topfivefoods():
+    print("taco")
+    print("pizza")
+    print("ribs)")
+
+def topfivevideogames():
+    print("sonic")
+    print("call of duty")
+    print("apex")
+
+def topfivesubjects():
+    print("math")
+    print("history")
+    print("programing")
+
+topfivevideogames()
+topfivefoods()
+topfivemovies()
+topfivesports()
+topfivesubjects()
+
+def mynameis(hamster):
+    print(hamster + "LaRose is my name")
+
+mynameis("LaRose")
+mynameis("Logan")
+
+def timesten(num):
+    print(num *10)
+
+timesten(17)
+
+def timesten(num):
+    print(num + 5)
+timesten(43)
+
+def timesten(num):
+    print(num / 2)
+
+timesten(7)
+
+def addtwonumbers(l,j):
+    print(x + y)
+
+addtwonumbers(10,59)
+
+def madlib(x,drum,day,l,minor):
+    print("While i've stayed at home, i've practiced the " + drum + "for" + eight + "hours every" + day + "my favorite key signature to play and practice in is" + l + minor + "it has" + nine + "flats and" + three + "sharps. I like this key signature because it is" + ugly + "and" + fat + "ive also gotten awesome at counting sigatures. The time signature I love is" + taco + "because it is" + awesome + "to count. One instrument i want to learn how to play while i'm at home is" + piano + ",because it is" + cool + "and sounds like a" + dog + "my" + sister + "likes it when i play" + Down + "and always gives me a round of applause after ,y performances! When i return to school, my teacher will be" + confused + "of how get great i am at playing my instrument. My teacher might even think i sound like" +polo + g + "!")
